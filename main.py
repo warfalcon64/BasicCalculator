384443a818a20724c9a61d48491913fe2d18453b
xXREACHXx <peppythepig8@gmail.com>
2021-05-02 11:15:27 -0500

    added main file and kv file

---
+++ main.py
@@ -0,0 +1,32 @@
+import kivy
+
+from kivy.app import App
+
+kivy.require('2.0.0')
+
+from kivy.config import GridLayout
+
+#Set size to resizeable
+Config.set('graphics', 'resizable', 1)
+
+calcApp = CalculatorApp()
+calcApp.run()
+
+#Making layout class
+class CalcGridLayout(GridLayout):
+
+    #Function is called when equal sign is pressed
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                #Solve formula and display it in entry which is pointed at by display
+                self.display.text = str(eval(calculation))
+
+            except Exception:
+                self.display.text = "Error"
+
+#Making App class
+class CalculatorApp(App):
+
+    def build(self):
+        return CalcGridLayout()
